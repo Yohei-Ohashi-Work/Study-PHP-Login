5a9703553ff5c7ae64d58973a11f31294058f0fc
ohashi-yohei@fujibuturyu.co.jp <hashi@MacBook-Pro-2.local>
2024-09-08 16:45:30 +0900

    ログイン機能をつくろう！ #05
    - ログイン機能完成とセッションハイジャック対策

--- public/login.php
+++ public/login.php
@@ -20,22 +20,25 @@ session_destroy();
 
 <body>
     <h2>ログインフォーム</h2>
+    <?php if (isset($err['msg'])) : ?>
+        <p><?php echo $err['msg']; ?></p>
+    <?php endif; ?>
     <form action="top.php" method="post">
-        <p>
+        <div>
             <label for="email">メールアドレス： </label>
             <input type="email" id="email" name="email">
             <?php if (isset($err['email'])) : ?>
-        <p><?php echo $err['email']; ?></p>
-    <?php endif; ?>
-    </p>
-    <p>
-        <label for="password">パスワード： </label>
-        <input type="password" id="password" name="password">
-        <?php if (isset($err['password'])) : ?>
-    <p><?php echo $err['password']; ?></p>
-<?php endif; ?>
-</p>
-<p><input type="submit" value='ログイン'></p>
+                <p><?php echo $err['email']; ?></p>
+            <?php endif; ?>
+        </div>
+        <div>
+            <label for="password">パスワード： </label>
+            <input type="password" id="password" name="password">
+            <?php if (isset($err['password'])) : ?>
+                <p><?php echo $err['password']; ?></p>
+            <?php endif; ?>
+        </div>
+        <p><input type="submit" value='ログイン'></p>
     </form>
     <a href="signup_form.php">新規登録はこちら</a>
 </body>
